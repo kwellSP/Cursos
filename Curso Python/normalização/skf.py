76fab5f701da93d92d3480b931fba5f8de159f1f
Ezequiel Almeida <ezequielmorato@bol.com.br>
2019-03-07 18:52:01 -0300

    Atualização 07-03-2019

--- Curso Python/normalização/skf.py
+++ Curso Python/normalização/skf.py
@@ -1,13 +1,62 @@
 import pandas as pd
 
-def trataReferencias(cod:object):
-    for index, row in referencia.loc[referencia['Código do Produto'] == cod].iterrows():
-        print('Codigo = {}  '.format(referencia['Código do Produto'][row]))
+def trataAplicacoes(cod:object, pos:int):
+    dados = applicacao.loc[applicacao['Código do Produto'] == cod]
+    if dados['Código do Produto'].count() == 1:
+        dfLista.at[pos, 'Linha'] = '{}'.format(dados.at[dados.index.item(), 'Tipo de Aplicação'])
+    elif dados['Código do Produto'].count() == 0:
+        dfLista.at[pos, 'Linha'] = ''
+    else:
+        consolidado = []
+        dfLista.at[pos, 'Linha'] = '{}'.format(dados.at[0, 'Tipo de Aplicação'])
+        for index, row in enumerate(dados.index):
+            consolidado.append('{}'.format(applicacao.at[row, 'Tipo de Aplicação']))
+        #dfLista.at[pos, 'Linha'] = consolidado
 
 
-#    for campo in referencia.loc[referencia['Código do Produto'] == cod]:
-#        for posicao,valor in enumerate(campo):
-#            print('{} = {} posição = {}'.format(campo, referencia[campo][posicao], posicao))
+def trataReferencias(cod:object, pos:int):
+    dados = referencia.loc[referencia['Código do Produto'] == cod]
+    if dados['Código do Produto'].count() == 1:
+        dfLista.at[pos, 'CodigoDaMontadora'] = '{}: {}'.format(referencia.at[dados.index.item(), 'Descrição do Fabricante do Veículo'],
+                                                                   referencia.at[dados.index.item(), 'Números Referência'])
+    elif dados['Código do Produto'].count() == 0:
+        dfLista.at[pos, 'CodigoDaMontadora'] = ''
+    else:
+        consolidado = []
+        for index, row in enumerate(dados.index):
+            consolidado.append('{}: {}'.format(referencia.at[row, 'Descrição do Fabricante do Veículo'],
+                                                   referencia.at[row, 'Números Referência']))
+        dfLista.at[pos, 'CodigoDaMontadora'] = consolidado
+
+
+def trataEquivalencias(cod:object, pos:int):
+    blackList = ['Cod. Barras', 'AGCO', 'Agrale', 'Alfa Romeo', 'Asia Motors', 'Audi', 'Baldan', 'BMW', 'CASE',
+                 'Caterpillar',
+                 'CBT', 'Chrysler', 'Citroen', 'CLASS', 'Colombo', 'Cross Lander', 'Daewoo', 'DAF', 'Dodge', 'Fiat',
+                 'FIAT ALLIS',
+                 'FNH', 'FNM', 'Ford', 'GM', 'GMC', 'Gurgel', 'Honda', 'Hyster', 'Hyundai', 'IFLÓ', 'International',
+                 'Iveco',
+                 'J.F.', 'Jac', 'Jacto', 'JAN', 'Jeep', 'John Deere', 'Jumil', 'Kia', 'Komatsu', 'Lada', 'Land Rover',
+                 'Man',
+                 'Marchesan', 'Marcopolo', 'MARRUCCI', 'Mazda', 'Mercedes-Benz', 'Mini', 'Mitsubishi', 'New Holland',
+                 'Nissan',
+                 'Peugeot', 'Porsche', 'Projelmec', 'Randon', 'Renault', 'Scania', 'Seat', 'STARA', 'Subaru', 'Suzuki',
+                 'Toyota',
+                 'Troller', 'Valtra', 'Vence Tudo', 'Volkswagen', 'Volvo', 'Yale']
+    dados = equivalencia.loc[(equivalencia['Código do Produto'] == cod) & (~equivalencia['Descrição do Fabricante'].isin(blackList))]
+    if dados['Código do Produto'].count() == 1:
+        dfLista.at[pos, 'CodigosSimilares'] = '{}: {}'.format(dados.at[dados.index.item(), 'Descrição do Fabricante'],
+                                                                   dados.at[dados.index.item(), 'Número Referência (Número Fabricante)'])
+    elif dados['Código do Produto'].count() == 0:
+        dfLista.at[pos, 'CodigosSimilares'] = ''
+    else:
+        consolidado = []
+        for index, row in enumerate(dados.index):
+            consolidado.append('{}: {}'.format(equivalencia.at[row, 'Descrição do Fabricante'],
+                                               equivalencia.at[row, 'Número Referência (Número Fabricante)']))
+        dfLista.at[pos, 'CodigosSimilares'] = consolidado
+
+
 
 
 #dicionário que irá conter todos os dados
@@ -41,12 +90,26 @@ descricao_ean = pd.read_excel('SKF_2.xlsx', sheet_name='Descrição+EAN', dtype=
 ncm_peso = pd.read_excel('SKF_2.xlsx', sheet_name='NCM+Peso', dtype={'Código do Produto': str, 'Montadora': str,
                                                                      'Aplicação Resumida': str, 'NCM': str, 'Peso bruto': str,
                                                                      'Comprimento': str, 'Largura': str, 'Altura': str})
+
+equivalencia = pd.read_excel('SKF_2.xlsx', sheet_name='Equivalencias 2', dtype={'Código do Produto': str, 'Descrição do Fabricante': str,
+                                                                                'Número Referência (Número Fabricante)': str})
+
+
 #remove caracters "-"
 ncm_peso = ncm_peso.replace('-', )
 
 
-equivalencia = pd.read_excel('SKF_2.xlsx', sheet_name='Equivalencias 2', dtype={'Código do Produto': str, 'Descrição do Fabricante': str,
-                                                                                'Número Referência (Número Fabricante)': str})
+applicacao['Tipo de Aplicação'] = applicacao['Tipo de Aplicação'].replace('Linha ', '')
+applicacao['Posição'] = applicacao['Posição'].replace('diant.', 'dianteiro').replace('tras.', 'traseiro').\
+    replace('inf.', 'inferior').replace('sup.', 'superior').replace('ext.', 'externo').replace('int.', 'interno').\
+    replace('interm.', 'intermediário').replace('veloc.', 'velocidade').\
+    replace('dir.', 'direita').replace('esq.', 'esquerda')
+applicacao['Grupo'] = applicacao['Grupo'].replace('Linha ', '')
+
+#capitaliza os dados dos campos necessários
+referencia['Descrição do Fabricante do Veículo'] = referencia['Descrição do Fabricante do Veículo'].str.capitalize()
+applicacao['Grupo'] = applicacao['Grupo'].str.capitalize()
+
 
 
 #apaga as colunas não utilizadas
@@ -68,8 +131,9 @@ dfLista['CodigoDoFabricante'] = unificados['Código do Produto'].unique()
 
 # padroniza as demais regras
 for pos, cod in enumerate(dfLista['CodigoDoFabricante']):
-    trataReferencias(cod)
-'''''''''
+    #trataAplicacoes(cod, pos)
+    trataEquivalencias(cod, pos)
+    trataReferencias(cod, pos)
     dfLista.at[pos, 'Nome'] = descricao_ean.loc[descricao_ean['Código do Produto'] == cod]['Descrição do Produto'].unique()
     dfLista.at[pos, 'EAN'] = list(descricao_ean.loc[descricao_ean['Código do Produto'] == cod]['EAN'])
     dfLista.at[pos, 'Marca'] = 'SKF'
@@ -106,10 +170,15 @@ for pos, cod in enumerate(dfLista['CodigoDoFabricante']):
     dfLista.at[pos, 'LarguraDoProduto'] = ncm_peso.loc[ncm_peso['Código do Produto'] == cod]['Largura'].unique()
     dfLista.at[pos, 'ComprimentoDoProduto'] = ncm_peso.loc[ncm_peso['Código do Produto'] == cod]['Comprimento'].unique()
     dfLista.at[pos, 'PesoBrutoDoProduto'] = ncm_peso.loc[ncm_peso['Código do Produto'] == cod]['Peso bruto'].unique()
-    
+    dfLista.at[pos, 'Linha'] = applicacao.loc[applicacao['Código do Produto'] == cod]['Tipo de Aplicação'].unique()
+    dfLista.at[pos, 'CategoriaFonte'] = applicacao.loc[applicacao['Código do Produto'] == cod]['Grupo'].unique()
+
 dfLista = dfLista.replace("'", '')
 dfLista = dfLista.replace('[', '')
 dfLista = dfLista.replace(']', '')
+dfLista['CodigosSimilares'] = dfLista['CodigosSimilares'].str.capitalize()
+
 dfLista.to_excel("resultado.xlsx")
-exit(-1)
-'''''
+
+
+
